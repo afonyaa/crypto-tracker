b1ce90a8f405aa9d29d84940a1d25850cb8a0e65
evgeny <afazhenya2001@gmail.com>
2024-05-02 21:20:11 +0300

    shared workers added

--- src/api/tickerUpdater.js
+++ src/api/tickerUpdater.js
@@ -1,14 +1,27 @@
 import { ADD_TICKER_TO_WS_LISTEN, REMOVE_TICKER_TO_WS_LISTEN } from '@/api/constants';
 import { getWSUrl } from '@/api/utils';
 
-class WSTicketListener {
+class WorkerTickerListener {
+  port;
+  constructor() {
+    const worker = new SharedWorker('shareTickersPricesWorker.js');
+    this.port = worker.port;
+  }
+  broadcastNewPrice({ tickerName, price }) {
+    this.port.postMessage({ tickerName, price });
+  }
+}
+
+class WSTickerListener {
   socket;
+  worker;
 
   constructor(url) {
     this.socket = new WebSocket(url);
+    this.worker = new WorkerTickerListener();
   }
 
-  sendMessage(tickerName, action) {
+  startListenCurrency(tickerName, action) {
     const message = JSON.stringify({ action: action, subs: [`5~CCCAGG~${tickerName}~USD`] });
     if (this.socket.readyState === WebSocket.OPEN) {
       this.socket.send(message);
@@ -22,25 +35,38 @@ class WSTicketListener {
     );
   }
 
-  startListening(tickerListeners) {
+  notifySubscribers(subscribers, { tickerName, price }) {
+    const handlers = subscribers.get(tickerName) ?? [];
+    handlers.forEach((handler) => handler(price));
+  }
+
+  activateIncomingEventsListener(tickerListeners) {
     this.socket.addEventListener('message', (e) => {
       const AGGREGATE_INDEX = '5';
-      const { TYPE: type, FROMSYMBOL: currency, PRICE: newPrice } = JSON.parse(e.data);
-      if (type !== AGGREGATE_INDEX) {
-        return;
+      const MULTIPLE_SOCKETS_ERROR = '429';
+
+      const { TYPE: type, FROMSYMBOL: tickerName, PRICE: price } = JSON.parse(e.data);
+
+      if (type === AGGREGATE_INDEX) {
+        this.worker.broadcastNewPrice({ tickerName, price });
+        this.notifySubscribers(tickerListeners, { tickerName, price });
       }
-      const handlers = tickerListeners.get(currency) ?? [];
-      handlers.forEach((handler) => handler(newPrice));
+      if (type === MULTIPLE_SOCKETS_ERROR) {
+        this.worker.port.onmessage = (e) => {
+          const { tickerName, price } = e.data;
+          this.notifySubscribers(tickerListeners, { tickerName, price });
+        };
+      }
     });
   }
 }
 
 class TickerSubscriber {
-  wsTickerListener = new WSTicketListener(getWSUrl());
+  wsTickerListener = new WSTickerListener(getWSUrl());
   tickerListeners = new Map();
 
   constructor() {
-    this.wsTickerListener.startListening(this.tickerListeners);
+    this.wsTickerListener.activateIncomingEventsListener(this.tickerListeners);
   }
 
   subscribeToTickerUpdates = (tickerName, listener) => {
@@ -49,7 +75,7 @@ class TickerSubscriber {
       listeners.push(listener);
     } else {
       this.tickerListeners.set(tickerName, [listener]);
-      this.wsTickerListener.sendMessage(tickerName, ADD_TICKER_TO_WS_LISTEN);
+      this.wsTickerListener.startListenCurrency(tickerName, ADD_TICKER_TO_WS_LISTEN);
     }
   };
   unsubscribeToTickerUpdates = (tickerName, unsubscribeListener) => {
@@ -59,7 +85,7 @@ class TickerSubscriber {
       listeners.filter((listener) => listener !== unsubscribeListener),
     );
     if (!this.tickerListeners.get(tickerName).length) {
-      this.wsTickerListener.sendMessage(tickerName, REMOVE_TICKER_TO_WS_LISTEN);
+      this.wsTickerListener.startListenCurrency(tickerName, REMOVE_TICKER_TO_WS_LISTEN);
     }
   };
 }
