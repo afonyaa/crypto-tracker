a371d4e8182b9e88f48d2bd1704205751175c17a
Evgeny Afanasyev <112726619+afonyaa@users.noreply.github.com>
2024-05-02 22:00:59 +0300

    Update constants.js

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,4 +1,4 @@
-export const API_KEY = 'f67d7ff5947dfec1152c5a985ac5ae995126a64abd4afcd3cce8d9ba80faa838';
+export const API_KEY = 'f67d7ff5947dfec1152c5a985ac5ae995126a64abd4afcd3cce8d9ba80faa838'; // отозван
 export const REST_URL = 'https://min-api.cryptocompare.com/';
 export const WS_URL = 'wss://streamer.cryptocompare.com/';
 
